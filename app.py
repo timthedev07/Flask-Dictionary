9a4137d1807e95f91d120feec79e3c496f02b302
Tim <harvard90873@gmail.com>
2021-12-05 13:22:16 +0100

    add and render words

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from sqlite3 import connect
-from constants import TABLE_NAME
+from constants import TABLE_NAME, DB_FILENAME
+
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 def index():
     stuff = []
 
-    with connect("db.db") as conn:
+    with connect(DB_FILENAME) as conn:
         db = conn.cursor()
         result = db.execute(f"SELECT * FROM {TABLE_NAME}")
 
@@ -17,3 +18,18 @@ def index():
             print(row)
 
     return render_template("index.html", rows=stuff)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    method = request.method
+    if method == "GET":
+        return render_template("add.html")
+    if method == "POST":
+        word = request.form.get("word")
+        definition = request.form.get("def")
+
+        with connect(DB_FILENAME) as conn:
+            db = conn.cursor()
+            result = db.execute(f"INSERT INTO {TABLE_NAME} (word, def) VALUES (?, ?)", (word, definition))
+
+        return redirect("/")

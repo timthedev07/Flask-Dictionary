9539bfdeff127a9200023c1335de2b1ed86cdacb
Tim <harvard90873@gmail.com>
2021-12-05 14:34:59 +0100

    add column

--- setup.sql
+++ setup.sql
@@ -1,1 +1,7 @@
-CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT UNIQUE, def TEXT NOT NULL, createdAt datetime default current_timestamp);
+CREATE TABLE IF NOT EXISTS words (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  word TEXT UNIQUE,
+  def TEXT NOT NULL,
+  createdAt datetime default current_timestamp,
+  wordType TEXT NOT NULL
+);
